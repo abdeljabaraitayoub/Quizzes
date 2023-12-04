a7de0aafdbc298c607a2ef4ca5a9307b4a9b1b91
abdeljabaraitayoub <abdeljabarayoubi@gmail.com>
2023-12-04 12:16:38 +0100

    i realize the add quizz

--- pages/quizzes/delete.php
+++ pages/quizzes/delete.php
@@ -1,10 +1,9 @@
 <?php
-require 'connection.php';
+include('../../dbcon.php');
 $id = $_GET['id'];
-$requet="DELETE FROM quizzes WHERE id = $id";
-$query=mysqli_query($con,$requet);
+$requet = "DELETE FROM quizzes WHERE id = $id";
+$query = mysqli_query($connection, $requet);
 
-if(isset($query)){
+if (isset($query)) {
     header("location:show.php");
 }
-?>
--- pages/quizzes/page.php
+++ pages/quizzes/page.php
@@ -1,35 +1,33 @@
 <?php
-    require 'connection.php';   
+include('../../dbcon.php');
 
- if(isset($_GET['id'])){
-     $quizzeTitle = $_POST['quizzeTitle'];
-     $courseId = $_POST['courseId'];
-     $score = $_POST['score'];
-     $date = $_POST['date'];
-     $id = $_POST['id'];
-     
-     $sql = "UPDATE `quizzes` SET quizzeTitle = '$quizzeTitle', course_id = '$courseId', score = '$score', `dateHour` = '$date' WHERE id = '$id'";
-     $query = mysqli_query($con,$sql);
+if (isset($_GET['id'])) {
+    $quizzeTitle = $_POST['quizzeTitle'];
+    $courseId = $_POST['courseId'];
+    $score = $_POST['score'];
+    $date = $_POST['date'];
+    $id = $_POST['id'];
 
-     if ($query) {
+    $sql = "UPDATE `quizzes` SET quizzeTitle = '$quizzeTitle', course_id = '$courseId', score = '$score', `dateHour` = '$date' WHERE id = '$id'";
+    $query = mysqli_query($connection, $sql);
+
+    if ($query) {
         header("location:show.php");
         exit;
     } else {
-        echo "Update failed: " . mysqli_error($con);
+        echo "Update failed: " . mysqli_error($connection);
     }
-   }
-   else {  
-   $quizzeTitle = $_POST['quizzeTitle'];
-   $courseId = $_POST['courseId'];
-   $score = $_POST['score'];
-   $date = $_POST['date'];
+} else {
+    $quizzeTitle = $_POST['quizzeTitle'];
+    $courseId = $_POST['courseId'];
+    $score = $_POST['score'];
+    $date = $_POST['date'];
 
-   $requet = "INSERT INTO  quizzes (`quizzeTitle` , `course_id` , `score` , `dateHour`) VALUE 
+    $requet = "INSERT INTO  quizzes (`quizzeTitle` , `course_id` , `score` , `dateHour`) VALUE 
    ('$quizzeTitle' , '$courseId' , '$score' , '$date')";
-   $query = mysqli_query($con,$requet);
+    $query = mysqli_query($connection, $requet);
 
-   if (isset($query)){
-    header("location:show.php");
-   }
- }
-   ?>
+    if (isset($query)) {
+        header("location:show.php");
+    }
+}
