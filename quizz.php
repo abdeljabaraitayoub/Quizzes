368e2c99163758833e78f44330ab5756b1d73d4e
elbazomaima <oumacode01.com>
2023-12-05 09:41:10 +0100

    connection problem fixed

--- quizz.php
+++ quizz.php
@@ -21,38 +21,29 @@
 <body>
 
 <form action="">
-<div class="container">
-  <div class="row">
-    <div class="col-md-8 offset-md-2">
-      <h2 class="text-center">QUiZZe</h2>
-      <?php
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h2 class="text-center">QUiZZe</h2>
+        <?php
 
-     require 'dbcon.php';
-     $sql = "SELECT FROM questions";
-     $result = mysqli_query($dbcon,$sql);
-      while ($value = mysqli_fetch_assoc($result)){
-     echo "<div class='question'>" . $value['question_text'] . "</div>";
-     $sql = "SELECT * FROM questions INNER JOIN answers ON questions.id = answers.question_id WHERE questions.id = " . $value['id']     ;
-     $result =mysqli_query($dbcon,$sql);
+          require 'dbcon.php';
+          $sql = "SELECT * FROM questions";
+          $result = mysqli_query($connection, $sql);
 
-     while ($value2 = mysqli_fetch_assoc($result)){
-     echo "<div class='option'>
-       <input type='radio' id=" . $value2['id'] . " name=" . $value['id'] . ">
-       <label for=" . $value2['id'] . ">" . $value2['answer_text'] . "</label>
-     </div>";
-   }
-    } 
+          while ($value = mysqli_fetch_assoc($result)) {
+            echo "<div class='question'>" . $value['question_text'] . "</div>";
+            $sql_answers = "SELECT * FROM answers WHERE question_id = " . $value['id'];
+            $result_answers = mysqli_query($connection, $sql_answers);
 
-   ?>
-      <div class="text-center">
-        <button class="btn btn-primary">Submit</button>
-      </div>
-    </div>x
-  </div>
-</div>
-</form>
+            while ($value2 = mysqli_fetch_assoc($result_answers)) {
+              echo "<div class='option'>
+                      <input type='radio' id=" . $value2['id'] . " name=" . $value['id'] . ">
+                      <label for=" . $value2['id'] . ">" . $value2['answer_text'] . "</label>
+                    </div>";
+            }
+          }
 
-<!-- Bootstrap JS -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
+        ?>
+        <div class="text-center">
+          <button class="btn btn-primary">Submit</button
